f84388bae2aa86cbcea12d89537f07b6cb1236c8
Aymeric <aymericbertrand.pro@gmail.com>
2021-03-29 10:57:05 +0200

    Added game function

--- js/scripts.js
+++ js/scripts.js
@@ -26,24 +26,43 @@ function playRound() {
     if (playerSelection == computerSelection) {
         return 'Tie';
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        return 'You Lose! Paper beats Rock';
-    }
     else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
         return 'You win! Rock beats Scissors';
     }
     else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
         return 'You win! Paper beats Rock';
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        return 'You lose! Scissors beats Paper';
-    }
     else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
         return 'You win! Scissors beats Paper';
+    }
+    else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        return 'You Lose! Paper beats Rock';
+    }
+    else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        return 'You lose! Scissors beats Paper';
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
         return 'You lose! Rock beats Scissors';
     }
 }
 
-alert(playRound());
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    playRound();
+    if (playRound == 'You win! Rock beats Scissors' || 'You win! Paper beats Rock' || 'You win! Scissors beats Paper') {
+        playerScore += 1;
+        alert("You scored 1 point");
+    }
+    else if (playRound == 'tie') {
+        playerScore += 0;
+        computerScore += 0;
+        alert("No point awarded");
+    }
+    else if (playRound == 'You Lose! Paper beats Rock' || 'You Lose! Paper beats Rock' || 'You Lose! Paper beats Rock') {
+        computerScore += 1;
+        alert("Your opponent scored 1 point");
+    }
+}
+
+alert(game());
